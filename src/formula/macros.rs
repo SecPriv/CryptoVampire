a0b6c1ab1204663cc9dcd58a6c327e1b23c1f9dd
Simon Jeanteur <7871851+puyral@users.noreply.github.com>
2023-07-31 22:11:23 +0200

    deleted useless files

--- src/formula/macros.rs
+++
@@ -1,9 +0,0 @@
-macro_rules! fun {
-    ($f:expr; $($arg:expr),*) => {
-        crate::formula::formula::RichFormula::Fun($f.clone(), vec![$($arg,)*])
-    };
-    ($f:pat, $args:pat) => {
-        crate::formula::formula::RichFormula::Fun($f, $args)
-    };
-}
-pub(crate) use fun;
