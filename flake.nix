6c9ce170dd8a0e83a2c50139156d936d89254d49
Simon Jeanteur <simon.jeanteur@tuwien.ac.at>
2024-04-22 12:59:21 +0200

    update nix

--- flake.nix
+++ flake.nix
@@ -4,13 +4,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils = { url = "github:numtide/flake-utils"; };
+    custom = {url = "github:puyral/custom-nix"; inputs.nixpkgs.follows = "nixpkgs";};
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+  outputs = inputs@{ self, nixpkgs, flake-utils, custom, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         src = ./cryptovampire;
+	custom-pkgs = custom.packages.${system};
         manifest = (pkgs.lib.importTOML "${src}/Cargo.toml").package;
       in rec {
         packages.cryptovampire = pkgs.rustPlatform.buildRustPackage {
@@ -26,7 +28,7 @@
 
           buildInputs = with pkgs;
             defaultPackage.buildInputs
-            ++ [ cargo rustc nil z3 cvc5 vampire rustfmt clippy rust-analyzer ];
+            ++ [ cargo rustc nil z3 cvc5 custom-pkgs.vampire-master rustfmt clippy rust-analyzer ];
         };
 
         defaultPackage = packages.cryptovampire;
