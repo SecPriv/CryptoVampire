2cdbf22a5d57aad0f6e83cf19004e3c38bec28b1
Simon Jeanteur <simon.jeanteur@tuwien.ac.at>
2024-07-24 18:09:37 +0200

    monads and aoo

---
+++ utils/src/all_or_one.rs
@@ -0,0 +1,276 @@
+use std::{error::Error, fmt::Debug, marker::PhantomData, ops::DerefMut};
+
+use if_chain::if_chain;
+use itertools::Itertools;
+use std::ops::Deref;
+
+use crate::monad::{Monad, MonadFamily, MonadFamilyMember};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum AllOrOne<U, V> {
+    All(V),
+    Any(U),
+    One(usize, U),
+}
+
+pub type AllOrOneShape = AllOrOne<(), usize>;
+
+pub type AoOV<U> = AllOrOne<U, Vec<U>>;
+
+impl AllOrOneShape {
+    pub fn try_merge(self, other: Self) -> Option<Self> {
+        match (self, other) {
+            (Self::One(_, ()), _) | (_, Self::One(_, ())) => {
+                if_chain! {
+                    if let Self::One(i, _) = self;
+                    if let Self::One(j, _) = other;
+                    if i == j;
+                    then {
+                        Some(Self::One(i, ()))
+                    } else {
+                        None
+                    }
+                }
+            }
+            (Self::All(i), Self::All(j)) => {
+                if i == j {
+                    Some(Self::All(i))
+                } else {
+                    None
+                }
+            }
+            (Self::All(i), _) | (_, Self::All(i)) => Some(Self::All(i)),
+            (Self::Any(()), Self::Any(())) => Some(Self::Any(())),
+        }
+    }
+
+    pub fn merge(self, other: Self) -> Self {
+        self.try_merge(other).unwrap()
+    }
+
+    pub fn monad_continue<U2>(self, mut f: impl FnMut() -> AoOV<U2>) -> AoOV<U2> {
+        match self {
+            AllOrOne::All(l) => {
+                AllOrOne::All((0..l).into_iter().map(|i| f().owned_get(i)).collect())
+            }
+            AllOrOne::Any(_) => {
+                let r = f();
+                assert!(matches!(r, AllOrOne::All(_) | AllOrOne::Any(_)));
+                r
+            }
+            AllOrOne::One(i, _) => AllOrOne::One(i, f().owned_get(i)),
+        }
+    }
+
+    pub fn to_aoov(self) -> AoOV<()> {
+        match self {
+            AllOrOne::All(size) => AllOrOne::All(vec![(); size]),
+            AllOrOne::Any(()) => AllOrOne::Any(()),
+            AllOrOne::One(i, ()) => AllOrOne::One(i, ()),
+        }
+    }
+}
+
+impl<U, V> AllOrOne<U, V>
+where
+    V: AsRef<[U]>,
+{
+    pub fn shape(&self) -> AllOrOneShape {
+        match self {
+            AllOrOne::All(l) => AllOrOne::All(l.as_ref().len()),
+            AllOrOne::Any(_) => AllOrOne::Any(()),
+            AllOrOne::One(i, _) => AllOrOne::One(*i, ()),
+        }
+    }
+
+    pub fn as_ref<'a>(&'a self) -> AllOrOne<&'a U, &'a [U]> {
+        match self {
+            AllOrOne::All(l) => AllOrOne::All(l.as_ref()),
+            AllOrOne::Any(u) => AllOrOne::Any(u),
+            AllOrOne::One(i, u) => AllOrOne::One(*i, u),
+        }
+    }
+}
+
+impl<U, V> AllOrOne<U, V>
+where
+    V: Deref<Target = [U]>,
+{
+    pub fn get(&self, i: usize) -> Option<&U> {
+        match self {
+            AllOrOne::All(l) => l.get(i),
+            AllOrOne::Any(x) => Some(x),
+            AllOrOne::One(j, x) if i == *j => Some(x),
+            _ => None,
+        }
+    }
+
+    pub fn len(&self) -> Option<usize> {
+        match self {
+            Self::All(l) => Some(l.len()),
+            _ => None,
+        }
+    }
+}
+
+impl<U, V> AllOrOne<U, V>
+where
+    V: DerefMut<Target = [U]>,
+{
+    pub fn get_mut(&mut self, i: usize) -> Option<&mut U> {
+        match self {
+            AllOrOne::All(l) => l.get_mut(i),
+            AllOrOne::Any(x) => Some(x),
+            AllOrOne::One(j, x) if i == *j => Some(x),
+            _ => None,
+        }
+    }
+}
+
+impl<U, V> AllOrOne<U, V>
+where
+    V: IntoIterator<Item = U>,
+{
+    pub fn map<F, U2, V2>(self, mut f: F) -> AllOrOne<U2, V2>
+    where
+        V2: FromIterator<U2>,
+        F: FnMut(U) -> U2,
+    {
+        match self {
+            AllOrOne::All(l) => AllOrOne::All(l.into_iter().map(f).collect()),
+            AllOrOne::Any(x) => AllOrOne::Any(f(x)),
+            AllOrOne::One(i, x) => AllOrOne::One(i, f(x)),
+        }
+    }
+}
+
+impl<U: Default, V> Default for AllOrOne<U, V> {
+    fn default() -> Self {
+        AllOrOne::Any(Default::default())
+    }
+}
+impl<U> AoOV<U> {
+    pub fn owned_get(self, i: usize) -> U {
+        match self {
+            AllOrOne::All(mut l) => l.swap_remove(i),
+            AllOrOne::Any(x) => x,
+            AllOrOne::One(j, x) if i == j => x,
+            _ => panic!("out of bounds"),
+        }
+    }
+
+    /// likely to panic if f return a `One` or an array that is not long enough
+    pub fn monad_continue<U2>(self, mut f: impl FnMut(U) -> AoOV<U2>) -> AoOV<U2> {
+        match self {
+            AllOrOne::All(l) => AllOrOne::All(
+                l.into_iter()
+                    .enumerate()
+                    .map(|(i, e)| f(e).owned_get(i))
+                    .collect(),
+            ),
+            AllOrOne::Any(e) => {
+                let r = f(e);
+                assert!(matches!(r, AllOrOne::All(_) | AllOrOne::Any(_)));
+                r
+            }
+            AllOrOne::One(i, e) => AllOrOne::One(i, f(e).owned_get(i)),
+        }
+    }
+}
+
+impl<U: Clone> AoOV<U> {
+    pub fn transpose_array<const N: usize>(args: [Self; N]) -> AoOV<[U; N]> {
+        if N == 0 {
+            AllOrOne::All(vec![])
+        } else {
+            let goal = args
+                .iter()
+                .map(|x| x.shape())
+                .fold(AllOrOne::default(), AllOrOne::merge);
+
+            match goal {
+                AllOrOne::All(size) => AllOrOne::All(
+                    (0..size)
+                        .map(|i| std::array::from_fn(|j| args[j].get(i).unwrap().clone()))
+                        .collect(),
+                ),
+                AllOrOne::Any(_) => AllOrOne::Any(args.map(|x| x.owned_get(0))),
+                AllOrOne::One(i, _) => AllOrOne::One(i, args.map(|x| x.owned_get(i))),
+            }
+        }
+    }
+}
+
+pub struct AllOrOneMonadicFamilly;
+
+impl MonadFamily for AllOrOneMonadicFamilly {
+    type Member<T> = AoOV<T>;
+}
+
+impl<T> MonadFamilyMember<T> for AoOV<T> {
+    type Of = AllOrOneMonadicFamilly;
+}
+
+impl<T> Monad<T> for AoOV<T> {
+    fn pure(u: T) -> Self {
+        AllOrOne::Any(u)
+    }
+
+    fn bind<B>(self, f: impl FnMut(T) -> AoOV<B>) -> AoOV<B> {
+        self.monad_continue(f)
+    }
+}
+
+pub struct AllOrOneResultMonadicFamilly<E: Error>(PhantomData<E>);
+
+impl<E: Error> MonadFamily for AllOrOneResultMonadicFamilly<E> {
+    type Member<T> = Result<AoOV<T>, E>;
+}
+
+impl<T, E: Error> MonadFamilyMember<T> for Result<AoOV<T>, E> {
+    type Of = AllOrOneResultMonadicFamilly<E>;
+}
+
+impl<T, E: Error> Monad<T> for Result<AoOV<T>, E> {
+    fn pure(u: T) -> Self {
+        Ok(AllOrOne::pure(u))
+    }
+
+    fn bind<B>(self, mut f: impl FnMut(T) -> Result<AoOV<B>, E>) -> Result<AoOV<B>, E> {
+        let s = self?;
+        Ok(match s {
+            AllOrOne::All(l) => {
+                let l = l
+                    .into_iter()
+                    .map(f)
+                    .enumerate()
+                    .map(|(i, e)| e.map(|x| x.owned_get(i)))
+                    .try_collect()?;
+                AllOrOne::All(l)
+            }
+            AllOrOne::Any(e) => {
+                let r = f(e)?;
+                assert!(matches!(r, AllOrOne::All(_) | AllOrOne::Any(_)));
+                r
+            }
+            AllOrOne::One(i, e) => AllOrOne::One(i, f(e)?.owned_get(i)),
+        })
+    }
+}
+
+fn flip_result<T, E>(s: AoOV<Result<T, E>>) -> Result<AoOV<T>, E> {
+    match s {
+        AllOrOne::All(l) => {
+            let l = l.into_iter().try_collect()?;
+            Ok(AllOrOne::All(l))
+        }
+        AllOrOne::Any(r) => {
+            let r = r?;
+            Ok(AllOrOne::Any(r))
+        }
+        AllOrOne::One(i, r) => {
+            let r = r?;
+            Ok(AllOrOne::One(i, r))
+        }
+    }
+}
---
+++ utils/src/monad.rs
@@ -0,0 +1,87 @@
+//thank you https://stackoverflow.com/a/78171691/10875409
+pub trait MonadFamily {
+    type Member<T>: Monad<T>;
+}
+
+pub trait MonadFamilyMember<T> {
+    type Of: MonadFamily<Member<T> = Self>;
+}
+pub trait Monad<A>: MonadFamilyMember<A> {
+    fn pure(u: A) -> Self;
+    fn bind<B>(
+        self,
+        f: impl FnMut(A) -> <Self::Of as MonadFamily>::Member<B>,
+    ) -> <Self::Of as MonadFamily>::Member<B>;
+}
+
+
+
+#[macro_export]
+macro_rules! mdo {
+  (pure $e:expr ) => {$crate::monad::Monad::pure($e)};
+  (let!($t:ty) $v:ident = $e:expr; $($rest:tt)*) => {
+    <$t as $crate::monad::Monad<_>>::pure($e)
+      .bind(move |$v| { $crate::mdo!($($rest)*)} )
+  };
+  (let!($t:ty) $v:ident : $t2:ty = $e:expr; $($rest:tt)*) => {
+    <$t as $crate::monad::Monad<$t2>>::pure($e)
+      .bind(move |$v| { $crate::mdo!($($rest)*)} )
+  };
+  (let!($t:ty) [ $($v:ident),* ] : $t2:ty = $e:expr; $($rest:tt)*) => {
+    <$t as $crate::monad::Monad<[$t2; _]>>::pure($e)
+      .bind(move |[$($v),*]| { $crate::mdo!($($rest)*)} )
+  };
+  (_ <- $monad:expr ; $($rest:tt)* ) => {
+    ($monad).bind( move |_| { mdo!($($rest)*)})
+  };
+  ($v:ident <- pure $e:expr ; $($rest:tt)* ) => {
+    $crate::monad::pure($e).bind( move |$v| { $crate::mdo!($($rest)*)})
+  };
+  ($v:ident <- $monad:expr ; $($rest:tt)* ) => {
+    ($monad).bind( move |$v| { $crate::mdo!($($rest)*)})
+  };
+  ([$($v:ident),*] <- $monad:expr ; $($rest:tt)* ) => {
+    ($monad).bind( move |[$($v),*]| { $crate::mdo!($($rest)*)})
+  };
+  (block $monad:block) => {$monad};
+  ($monad:expr;!) => {$monad};
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    pub struct VecFamily;
+
+    impl MonadFamily for VecFamily {
+        type Member<T> = Vec<T>;
+    }
+
+    impl<T> MonadFamilyMember<T> for Vec<T> {
+        type Of = VecFamily;
+    }
+
+    impl<A> Monad<A> for Vec<A> {
+        fn pure(u: A) -> Self {
+            vec![u]
+        }
+
+        fn bind<B>(
+            self,
+            f: impl FnMut(A) -> <Self::Of as MonadFamily>::Member<B>,
+        ) -> <Self::Of as MonadFamily>::Member<B> {
+            self.into_iter().flat_map(f).collect()
+        }
+    }
+
+    #[test]
+    fn test() {
+        let array = mdo! {
+          let!(Vec<i32>) x = 3 ;
+          a <- vec![4, 5];
+          [b] <- vec![[6], [7]];
+          pure (x * a * b)
+        };
+        assert_eq!(vec![3 * 4 * 6, 3 * 4 * 7, 3 * 5 * 6, 3 * 5 * 7], array)
+    }
+}
