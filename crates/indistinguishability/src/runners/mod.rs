31347c28e40fefdaa0df83e89d7bb45c95d580ad
Simon Jeanteur <simon.jeanteur@tuwien.ac.at>
2025-10-31 16:05:28 +0100

    prepare for gemini

--- crates/indistinguishability/src/runners/mod.rs
+++ crates/indistinguishability/src/runners/mod.rs
@@ -16,27 +16,26 @@ pub trait SmtSolver: Debug {
     /// - `Ok(None)` the solver didn't manage to prove nor disprove the query
     /// - `Ok(b)` with `b` true is proven, `false` otherwise
     fn try_run(&self, pbl: &mut Problem, query: MSmtFormula) -> anyhow::Result<Option<bool>>;
+}
 
-    fn run_to_dependancy(&self, pbl: &mut Problem, query: MSmtFormula) -> Dependancy {
+#[derive(Debug, Clone)]
+pub struct SmtRunner(Rc<dyn SmtSolver>);
+
+impl SmtRunner {
+    pub fn new(pbl: &Problem) -> Self {
+        Self(Rc::new(RegularVampire::new(pbl)))
+    }
+
+    pub fn run_to_dependancy(&self, pbl: &mut Problem, query: MSmtFormula) -> Dependancy {
         if let Some(true) = self.try_run(pbl, query).unwrap() {
             Dependancy::axiom()
         } else {
             Dependancy::impossible()
         }
     }
-}
 
-#[derive(Debug, Clone)]
-pub struct SmtRunner(Rc<dyn SmtSolver>);
-
-impl SmtSolver for SmtRunner {
-    fn try_run(&self, pbl: &mut Problem, query: MSmtFormula) -> anyhow::Result<Option<bool>> {
+    /// TODO: make run many solvers
+    pub fn try_run(&self, pbl: &mut Problem, query: MSmtFormula) -> anyhow::Result<Option<bool>> {
         self.0.try_run(pbl, query)
-    }
-}
-
-impl SmtRunner {
-    pub fn new(pbl: &Problem) -> Self {
-        Self(Rc::new(RegularVampire::new(pbl)))
     }
 }
