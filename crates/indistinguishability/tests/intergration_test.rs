2f5d5216dd223567e5348737b2862862289195e9
Simon Jeanteur <simon.jeanteur@tuwien.ac.at>
2025-11-04 22:50:29 +0100

    tests

---
+++ crates/indistinguishability/tests/intergration_test.rs
@@ -0,0 +1,26 @@
+use std::{env, path::Path};
+
+use assert_cmd::{Command, cargo_bin};
+use predicates::prelude::*;
+
+#[test]
+fn basic_hash() {
+    mk_test("./tests/basic-hash.scm", &[]);
+}
+
+#[test]
+fn lak_tag() {
+    mk_test(
+        "./tests/lak-tag.scm",
+        &["--node-limit", "100000", "--prf-limit", "1"],
+    );
+}
+
+fn mk_test(file: impl AsRef<Path>, extra_args: &[&str]) {
+    let mut cmd = Command::new(cargo_bin!());
+    cmd.pipe_stdin(file).unwrap();
+    cmd.args(extra_args);
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("success"));
+}
