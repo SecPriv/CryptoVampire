84cdbd756dc11ac8d9fca32100d8d78d108e6b6d
Simon Jeanteur <simon.jeanteur@tuwien.ac.at>
2025-11-08 22:48:36 +0100

    need notion of public knowledge

--- crates/indistinguishability/tests/private-auth.scm
+++ crates/indistinguishability/tests/private-auth.scm
@@ -28,19 +28,19 @@
 (define ra (wrap-nonce _ra))
 (define rb (wrap-nonce _rb))
 
-(define (pka1 i) (pk (ka a1 i)))
-(define (pka2 i) (pk (ka a2 i)))
-(define (pkb i) (pk (kb i)))
-
 (define (ltrue . args) mtrue)
 
-(declare-step pbl "publish_a" (list Index Index)
-  (step p1 ltrue (lambda (in i j) (pk (ka i j))))
-  (step p2 ltrue (lambda (in i j) (pk (ka i j)))))
+(define pa (declare-step pbl "publish_a" (list Index Index)
+    (step p1 ltrue (lambda (in i j) (pk (ka i j))))
+    (step p2 ltrue (lambda (in i j) (pk (ka i j))))))
 
-(declare-step pbl "publish_b" (list Index)
-  (step p1 ltrue (lambda (in i) (pkb i)))
-  (step p2 ltrue (lambda (in i) (pkb i))))
+(define pb (declare-step pbl "publish_b" (list Index)
+    (step p1 ltrue (lambda (in i) (pk (kb i))))
+    (step p2 ltrue (lambda (in i) (pk (kb i))))))
+
+(define (pka1 i p) (macro_msg (pa a1 i) p))
+(define (pka2 i p) (macro_msg (pa a2 i) p))
+(define (pkb i p) (macro_msg (pb i) p))
 
 (define b1
   (declare-step pbl "b1" (list Index)
@@ -52,38 +52,39 @@
     (step p1 ltrue (lambda (in i)
         (let [ (in (dec in (kb i))) (dflt (tuple (nb i) (nb i))) ]
           (m_ite
-            (eq (sel1of2 in) (pka1 i))
+            (eq (sel1of2 in) (pka1 i p1))
             (m_ite (eql (tuple (sel2of2 in) (nb i)) dflt)
-              (enc (tuple (sel2of2 in) (nb i)) (rb i) (pka1 i))
-              (enc dflt (rb i) (pka1 i)))
-            (enc dflt (rb i) (pka1 i))))))
+              (enc (tuple (sel2of2 in) (nb i)) (rb i) (pka1 i p1))
+              (enc dflt (rb i) (pka1 i p1)))
+            (enc dflt (rb i) (pka1 i p1))))))
     (step p2 ltrue (lambda (in i)
         (let [ (in (dec in (kb i))) (dflt (tuple (nb i) (nb i))) ]
           (m_ite
-            (eq (sel1of2 in) (pka2 i))
+            (eq (sel1of2 in) (pka2 i p2))
             (m_ite (eql (tuple (sel2of2 in) (nb i)) dflt)
-              (enc (tuple (sel2of2 in) (nb i)) (rb i) (pka2 i))
-              (enc dflt (rb i) (pka2 i)))
-            (enc dflt (rb i) (pka2 i))))))))
+              (enc (tuple (sel2of2 in) (nb i)) (rb i) (pka2 i p2))
+              (enc dflt (rb i) (pka2 i p2)))
+            (enc dflt (rb i) (pka2 i p2))))))))
 
 (define as (declare-step pbl "as" (list Index Index)
-    (step p1 ltrue (lambda (in i j) (enc (tuple in (na i j)) (ra i j) (pkb j))))
-    (step p2 ltrue (lambda (in i j) (enc (tuple in (na i j)) (ra i j) (pkb j))))))
+    (step p1 ltrue (lambda (in i j) (enc (tuple in (na i j)) (ra i j) (pkb j p1))))
+    (step p2 ltrue (lambda (in i j) (enc (tuple in (na i j)) (ra i j) (pkb j p2))))))
 
 (initialize-as-aenc aenc enc dec pk)
 
-; (bind
-;   ((i Index) (j Index)
-;     (t Time)
-;     (p Protocol))
-;   (let [ (in (macro_input t p)) ]
-;     (cv-add-rewrite pbl (cv-mk-rewrite "lemma-2" (list i t j p)
-;         (eq (sel2of2 in) (mhash (sel1of2 in) (mk i j p)))
-;         (exists ((i Index))
-;           (cand
-;             (eq (sel1of2 in) (sel1of2 (macro_input (tag i j) p)))
-;             (eq (sel2of2 in) (sel2of2 (macro_input (tag i j) p)))
-;             (lt (tag i j) t))))))); <- very important
+(bind
+  ((i Index) (j Index) (k Index) (l Index))
+  (begin
+    (cv-add-rewrite pbl (cv-mk-rewrite "order-1" (list i j)
+        (lt (pb i) (as i j))
+        mtrue))
+    (cv-add-rewrite pbl (cv-mk-rewrite "order-2" (list i j)
+        (lt (pa i j) (b2 i))
+        mtrue))
+    ; (cv-add-rewrite pbl (cv-mk-rewrite "order-1" (list i j)
+    ;     (pred (pa i j) (as i j))
+    ;     mtrue))
+));
 
 (if (run pbl p1 p2)
   (displayln "success")
