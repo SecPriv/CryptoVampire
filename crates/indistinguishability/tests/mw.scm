f634bfd334eab38355c66c2718d0670d3256c9cc
Simon Jeanteur <simon.jeanteur@tuwien.ac.at>
2026-01-03 03:00:58 +0100

    20s

--- crates/indistinguishability/tests/mw.scm
+++ crates/indistinguishability/tests/mw.scm
@@ -128,7 +128,7 @@
 
 ;; configuration
 (cv-set-trace pbl #t)
-(cv-set-vampire-timeout pbl (cv-string->duration "15s"))
+(cv-set-vampire-timeout pbl (cv-string->duration "20s"))
 (cv-set-node-limit pbl 100000)
 (cv-set-prf-limit pbl 1)
 
