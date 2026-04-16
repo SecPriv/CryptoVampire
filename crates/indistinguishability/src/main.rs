75b0bec1291816ac0e22749fec5ee3075db2d0f4
Simon Jeanteur <simon.jeanteur@tuwien.ac.at>
2026-04-16 10:45:31 +0200

    better error exit

--- crates/indistinguishability/src/main.rs
+++ crates/indistinguishability/src/main.rs
@@ -6,6 +6,7 @@ use std::io::{self, Read};
 
 use clap::Parser;
 use indistinguishability::{Commands, Configuration, init_engine, init_logger};
+use steel::rerrs::ErrorKind;
 
 // static CV_PRELUDE: &str = include_str!("./input/prelude.scm");
 pub fn main() {
@@ -32,9 +33,27 @@ pub fn main() {
             };
 
             if let Err(e) = engine.run(pgrm.clone()) {
+                let err = to_error_code(&e.kind());
                 engine.raise_error(e);
-                panic!()
+                std::process::exit(err)
             }
         }
+    }
+}
+
+fn to_error_code(kind: &ErrorKind) -> i32 {
+    use ErrorKind::*;
+    match kind {
+        ArityMismatch => 1,
+        FreeIdentifier => 2,
+        TypeMismatch => 3,
+        UnexpectedToken => 4,
+        ContractViolation => 5,
+        BadSyntax => 6,
+        ConversionError => 7,
+        Io => 8,
+        Parse => 9,
+        Infallible => 10,
+        Generic => 11,
     }
 }
